3b254d9acf1a4af359b3de1a0b020f5b032b4480
Ben <benjaminctyner@gmail.com>
2023-06-25 08:27:41 -0400

    Complete sidebar.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './index.css';
+import SideBar from './components/SideBar';
+
+function App() {
+  return (
+    <div className='flex '>
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
